375cd3fb5545413c9ffd75f6da60ac71d47ba486
athenrome <athenrome@gmail.com>
2019-11-04 20:12:19 +1000

    Imported Player, Object, Input and Map Systems + basic materials

---
+++ Card Labyrinth/Assets/Player System/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerController
+{
+    static List<Unit> selectedUnits = new List<Unit>();
+
+    public static CameraController playerCam;
+
+    public static void SetupPlayer()
+    {
+        playerCam = GameObject.FindObjectOfType<CameraController>();
+    }
+
+    public static void PlayerUpdate()
+    {
+        HandlePlayerInputs();
+
+        if (Input.GetMouseButtonDown(1) == true)
+        {
+            RaycastHit hitPoint;
+
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPoint);
+
+            Vector3 moveClickPos = hitPoint.point;
+
+            if (selectedUnits.Count == 1)
+            {
+                selectedUnits[0].SetMoveTarget(moveClickPos);
+            }
+            else
+            {
+                ResolveGroupMovement(moveClickPos);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.LeftAlt) == true && Input.GetMouseButtonDown(0) == true)
+        {
+            RaycastHit hitPoint;
+
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPoint);
+            MapTile hitTile = hitPoint.collider.GetComponent<MapTile>();
+
+            hitTile.tileBase.GetComponent<MeshRenderer>().material.color = Color.yellow;
+
+
+            ObjectPool.RetrieveProp("Unit").transform.position = hitTile.centrePoint.position;
+        }
+    }
+
+    static void ResolveGroupMovement(Vector3 startPoint)
+    {
+
+    }
+
+    public static void SelectObject(SelectableObject selectedObj)
+    {
+        if(selectedObj.GetType() == typeof(Unit))
+        {
+            selectedUnits.Add((Unit)selectedObj);
+        }
+    }
+
+    public static void DeselectObject(SelectableObject selectedObj)
+    {
+        if (selectedObj.GetType() == typeof(Unit))
+        {
+            selectedUnits.Remove((Unit)selectedObj);
+        }
+    }
+
+    static void HandlePlayerInputs()
+    {
+        if (InputController.CheckInputEvent("DeselectAll") == true)
+        {
+            foreach (Unit unit in selectedUnits)
+            {
+                unit.OnDeselect();
+            }
+
+            selectedUnits = new List<Unit>();
+
+        }
+
+    }
+}
---
+++ Card Labyrinth/Assets/SelectableObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SelectableObject : MonoBehaviour
+{
+    public bool selected = false;
+
+    public abstract void OnSelect();
+
+    public abstract void OnDeselect();
+
+    void OnMouseDown()
+    {
+        if (selected == false)
+        {
+            OnSelect();
+            selected = true;
+        }
+        else if (selected == true && Input.GetKey(InputController.GetInputEventKey("Deselect")) == true)
+        {
+            OnDeselect();
+        }
+        else//Selected an already selected object - LookAt
+        {
+            PlayerController.playerCam.LookAtPos(this.transform.position);
+        }
+
+    }
+
+
+
+}
