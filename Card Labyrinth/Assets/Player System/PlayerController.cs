087204e796587fd98bf64e854a04f2bcd5006981
athenrome <athenrome@gmail.com>
2019-11-07 21:23:56 +1000

    Removed Unit References from PlayerController

--- Card Labyrinth/Assets/Player System/PlayerController.cs
+++ Card Labyrinth/Assets/Player System/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public static class PlayerController
 {
-    static List<Unit> selectedUnits = new List<Unit>();
-
     public static CameraController playerCam;
+
+    public static SelectableObject selectedObj;
+
+    public static Vector2 playerPos;
 
     public static void SetupPlayer()
     {
@@ -16,24 +18,6 @@ public static class PlayerController
     public static void PlayerUpdate()
     {
         HandlePlayerInputs();
-
-        if (Input.GetMouseButtonDown(1) == true)
-        {
-            RaycastHit hitPoint;
-
-            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPoint);
-
-            Vector3 moveClickPos = hitPoint.point;
-
-            if (selectedUnits.Count == 1)
-            {
-                selectedUnits[0].SetMoveTarget(moveClickPos);
-            }
-            else
-            {
-                ResolveGroupMovement(moveClickPos);
-            }
-        }
 
         if(Input.GetKey(KeyCode.LeftAlt) == true && Input.GetMouseButtonDown(0) == true)
         {
@@ -49,39 +33,23 @@ public static class PlayerController
         }
     }
 
-    static void ResolveGroupMovement(Vector3 startPoint)
+    public static void SelectObject(SelectableObject newSelection)
     {
-
+        selectedObj = newSelection;
     }
 
-    public static void SelectObject(SelectableObject selectedObj)
+    public static void DeselectObject()
     {
-        if(selectedObj.GetType() == typeof(Unit))
-        {
-            selectedUnits.Add((Unit)selectedObj);
-        }
+        selectedObj = null;
     }
 
-    public static void DeselectObject(SelectableObject selectedObj)
+    public static void MovePlayer(Vector2 targetTile)
     {
-        if (selectedObj.GetType() == typeof(Unit))
-        {
-            selectedUnits.Remove((Unit)selectedObj);
-        }
+
     }
 
     static void HandlePlayerInputs()
     {
-        if (InputController.CheckInputEvent("DeselectAll") == true)
-        {
-            foreach (Unit unit in selectedUnits)
-            {
-                unit.OnDeselect();
-            }
-
-            selectedUnits = new List<Unit>();
-
-        }
 
     }
 }
